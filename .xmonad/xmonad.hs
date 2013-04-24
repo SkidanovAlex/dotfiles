2f2c25493382eb771702435f39c6dd891f610ce2
alex <alex@memsql.com>
2013-04-23 19:10:45 -0700

    XMonad, screen and vimperator configs

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,9 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+
+ 
+main = xmonad =<< xmobar defaultConfig
+        {
+            modMask = mod4Mask,
+            focusFollowsMouse = False
+        }
